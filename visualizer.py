ad1802eb73736cbb5c7bada3ab468c0c9595a91b
Abdullah Kamran <abdullahk.1495@gmail.com>
2026-02-07 20:46:35 +0500

    data visualizer

---
+++ visualizer.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+
+def region_bar_chart(filtered_df, region, year):
+
+    plt.figure()
+    plt.bar(filtered_df["Country Name"], filtered_df["Value"])
+    plt.title(f"GDP of {region} ({year})")
+    plt.xlabel("Country")
+    plt.ylabel("GDP")
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.show()
+
+
+def region_pie_chart(filtered_df, region, year):
+
+    plt.figure()
+    plt.pie(filtered_df["Value"], labels=filtered_df["Country Name"], autopct="%1.1f%%")
+    plt.title(f"GDP Distribution of {region} ({year})")
+    plt.show()
+
+
+def yearly_histogram(df, year):
+
+    year_df = df[df["Year"] == year]
+
+    plt.figure()
+    plt.hist(year_df["Value"])
+    plt.title(f"GDP Histogram ({year})")
+    plt.xlabel("GDP")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+def yearly_scatter(df, year):
+
+    year_df = df[df["Year"] == year]
+
+    plt.figure()
+    plt.scatter(year_df["Country Name"], year_df["Value"])
+    plt.title(f"GDP Scatter Plot ({year})")
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.show()
